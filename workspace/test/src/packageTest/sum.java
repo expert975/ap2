6d1c20f69f97618cde93b32c55e529325c335bdb
expert975 <rodrigocomputadores125@hotmail.com>
2018-03-16 07:01:23 -0400

    Create workspace and add hello world

---
+++ workspace/test/src/packageTest/sum.java
@@ -0,0 +1,17 @@
+package packageTest;
+
+import java.util.Scanner;
+
+public class sum
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int number1;
+		int number2;
+		int sum;
+		
+		System.out.println("Enter fisrt integer: ");
+	}
+}

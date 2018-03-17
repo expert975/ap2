2536f379eb3cd1a925d44e5f39c9b87fd30a9f0f
expert975 <rodrigocomputadores125@hotmail.com>
2018-03-17 01:16:03 -0300

    Capitalize Sum class

---
+++ workspace/test/src/packageTest/Sum.java
@@ -0,0 +1,26 @@
+package packageTest;
+
+import java.util.Scanner;
+
+public class Sum
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int number1;
+		int number2;
+		int sum;
+		
+		System.out.print("Enter fisrt integer: ");
+		number1 = input.nextInt();
+		
+		System.out.print("Enter second integer: ");
+		number2 = input.nextInt();
+		
+		sum = number1 + number2;
+		
+		System.out.printf("Sum is: %d", sum);
+		input.close();	
+	}
+}
